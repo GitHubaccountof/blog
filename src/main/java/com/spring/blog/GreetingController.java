805081f69285ffbc4d9bfb332093dc29178eede6
Vladyslav Krys <vladyslav_krys@epam.com>
2019-12-01 15:00:17 +0200

    move file (html)

--- src/main/java/com/spring/blog/GreetingController.java
+++ src/main/java/com/spring/blog/GreetingController.java
@@ -9,10 +9,7 @@ import java.util.Map;
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    ) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
